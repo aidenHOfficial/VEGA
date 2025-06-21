7ab50340bf1888272f5694458960f9bf0f41dc01
aidenHOfficial <aiden.b.humphri3s@gmail.com>
2025-06-21 10:45:17 -0600

    Started project and added ascii title

---
+++ main.py
@@ -0,0 +1,19 @@
+def main():
+    print(ascii_title)
+
+    responsibilities = []
+    goals = []
+    ideas = []
+    wants = []
+    preferences = []
+
+ascii_title = """
+        _    _________________ 
+       | |  / / ____/ ____/   |
+       | | / / __/ / / __/ /| |
+       | |/ / /___/ /_/ / ___ |
+       |___/_____/\____/_/  |_|
+Personalized Schedule Builder And Assistant\n\n"""
+
+if __name__ == "__main__":
+    main()
